2a065f53600ebac41cf28d7655a514f9ca811b70
kevaf <kevinkipyegon20@gmail.com>
2021-09-04 12:13:47 +0300

    added users class to main file

---
+++ main.py
@@ -0,0 +1,12 @@
+
+class Users:
+    """
+    Class that generates new instances of Users
+    """
+    users = []
+
+    def __init__(self, main_user, main_password):
+        
+        self.main_user = main_user
+        self.main_password = main_password
+
