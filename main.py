8d4bea12ba66c06bc1296818557fca8def4034ea
kevaf <kevinkipyegon20@gmail.com>
2021-09-04 12:37:54 +0300

    added method to save new user

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ class Users:
     """
     Class that generates new instances of Users
     """
-    users = []
+    users_list = []
 
     def __init__(self, main_user, main_password):
         
         self.main_user = main_user
         self.main_password = main_password
+    
+    def add_user(self):
+        """
+        method that adds a new user object to users
+        """
+        Users.users_list.append(self)
 
